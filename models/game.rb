3a1cb32a5cb72f87530a8e359893c22552cf8afa
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-22 01:16:06 +0100

    .play_game created and tested

--- models/game.rb
+++ models/game.rb
@@ -8,3 +8,30 @@ class Game
     @lefthand_score = 0
     @righhand_score = 0
   end
+
+  def play_game()
+
+    if @lefthand == "scissors" && @righthand == "rock"
+      return "Rock wins!"
+    elsif @lefthand == "rock" && @righthand == "scissors"
+      return "Rock wins!"
+    elsif @lefthand == "scissors" && @righthand == "paper"
+      return "Scissors win!"
+    elsif @lefthand == "paper" && @righthand == "scissors"
+      return "Scissors win!"
+    elsif @lefthand == "paper" && @righthand == "rock"
+      return "Paper wins!"
+    else @lefthand == "rock" && @righthand == "paper"
+      return "Paper wins!"
+    # elsif @lefthand == @righthand
+    #   return "Tie!"
+    end
+
+
+    #   if @lefthand == ("scissors" || "rock") || @righthand == "scissors" #|| "rock")
+    #   return "Rock wins!"
+    # end
+
+  end
+
+end
--- specs/game_specs.rb
+++ specs/game_specs.rb
@@ -1,14 +1,53 @@
 require 'minitest/autorun'
 require_relative '../models/game'
 
-class GameCalculator < Minitest::Test
+class TestGame < Minitest::Test
 
   def setup
-    game1 = Game.new("scissors", "rock")
-    game2 = Game.new("scissors", "paper")
-    game3 = Game.new("rock", "paper")
+    @game1 = Game.new("scissors", "rock")
+    @game2 = Game.new("rock", "scissors")
+    @game3 = Game.new("scissors", "paper")
+    @game4 = Game.new("paper", "scissors")
+    @game5 = Game.new("rock", "paper")
+    @game6 = Game.new("paper", "rock")
+    @game7 = Game.new("paper", "paper")
+    @game8 = Game.new("rock", "rock")
+    @game6 = Game.new("scissors", "scissors")
   end
 
-  
+  def test_play_game__scissors_rock()
+    @game1.play_game()
+    assert_equal("Rock wins!", @game1.play_game())
+  end
+
+  def test_play_game__rock_scissors()
+    @game2.play_game()
+    assert_equal("Rock wins!", @game2.play_game())
+  end
+
+  def test_play_game__scissors_paper()
+    @game3.play_game()
+    assert_equal("Scissors win!", @game3.play_game())
+  end
+
+  def test_play_game__paper_scissors()
+    @game4.play_game()
+    assert_equal("Scissors win!", @game4.play_game())
+  end
+
+  def test_play_game__rock_paper()
+    @game5.play_game()
+    assert_equal("Paper wins!", @game5.play_game())
+  end
+
+  def test_play_game__paper_rock()
+    @game6.play_game()
+    assert_equal("Paper wins!", @game6.play_game())
+  end
+
+  # def test_play_game__tie_1
+  #   @game7.play_game()
+  #   assert_equal("Tie!", @game7.play_game())
+  # end
 
 end
